04c8fdd86c320a4a204b75d33c9e8a35c8cdacd1
Hardik Mayani <121415300+chatterjee-design@users.noreply.github.com>
2024-12-25 14:53:01 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+import  express  from "express";
+import {config } from "dotenv";
+import errorMiddleware from "./Middlewares/error.middleware.js";
+import cookieParser from "cookie-parser";
+import {v2 as cloudinary} from 'cloudinary';
+import userRouter from "./Routes/user.routes.js";
+
+
+//config doten file
+config()
+
+const app = express();
+
+// Middleware setup
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+//cloudinary setup for uploading files to cloudinary         
+cloudinary.config({ 
+  cloud_name: process.env.CLOUD_NAME, 
+  api_key: process.env.API_KEY, 
+  api_secret: process.env.API_SECRET 
+});
+
+
+
+
+//router setup
+app.use('/api/user', userRouter);
+
+
+
+//if page not found
+app.all('*', (req, res) => {
+    res.status(404).send('OOPS! 404 NOT FOUND');
+})
+
+//error handling/ if any error happens
+app.use(errorMiddleware)
+
+export default app;
